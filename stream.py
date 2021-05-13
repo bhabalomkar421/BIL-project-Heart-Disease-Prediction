59ce4798fbd808ec69900f13c65710979cc3250f
Bhabalomkar421 <Omkarbhabal11@gmail.com>
2021-05-13 18:07:53 +0530

    Load UI added

--- stream.py
+++ stream.py
@@ -4,16 +4,49 @@ import numpy as np
 import pickle
 filename = 'finalized_model1.sav'
 
-def add():
+def predict():
     return 'add function'
 
 def load_UI():
-    st.text('Test')
-    
+    """ loads the GUI """
+    st.title('Heart Disease Prediction')
 
-    x = st.button('add')
+    age = st.text_input('Enter Age')
+
+    sex = st.text_input('Enter Sex')
+
+    cp = st.text_input('Enter CP')
+
+    trestbps = st.text_input('Enter trestbps')
+
+    chol = st.text_input('Enter chol')
+
+    fbs = st.text_input('Enter fbs')
+
+    restecg = st.text_input('Enter restecg')
+
+    thalach = st.text_input('Enter thalach')
+
+    exang = st.text_input('Enter exang')
+
+    oldpeak = st.text_input('Enter oldpeak')
+
+    slope = st.text_input('Enter slope')
+
+    ca = st.text_input('Enter ca')
+
+    thal = st.text_input('Enter thal')
+
+    x = st.button('predict')
     if(x):
-        st.text(x)
+        if(age and sex and cp and trestbps and chol and fbs and restecg and thalach and exang and oldpeak and slope and ca and thal):
+            input = np.array([age,sex,cp,trestbps,chol,fbs,restecg,thalach,exang,oldpeak,slope,ca,thal])
+            if(pred(input)[0] == 0):
+                st.text('Your are Safe, You don\'t have any Heart Disease !!! Stay Safe Track your health Daily')
+            else:  
+                st.text('Your report says that you haveHeart Disease')
+        else:
+            st.text('Please fill all the fields!!!') 
 
 def pred(input):
     print("loading saved model")
@@ -24,43 +57,3 @@ def pred(input):
 
 
 load_UI()
-
-# st.title('Heart Disease Prediction')
-
-# age = st.text_input('Enter Age')
-
-# sex = st.text_input('Enter Sex')
-
-# cp = st.text_input('Enter CP')
-
-# trestbps = st.text_input('Enter trestbps')
-
-# chol = st.text_input('Enter chol')
-
-# fbs = st.text_input('Enter fbs')
-
-# restecg = st.text_input('Enter restecg')
-
-# thalach = st.text_input('Enter thalach')
-
-# exang = st.text_input('Enter exang')
-
-# oldpeak = st.text_input('Enter oldpeak')
-
-# slope = st.text_input('Enter slope')
-
-# ca = st.text_input('Enter ca')
-
-# thal = st.text_input('Enter thal')
-
-# if(age and sex and cp and trestbps and chol and fbs and restecg and thalach and exang and oldpeak and slope and ca and thal):
-#     print(age,sex,cp,trestbps,chol,fbs,restecg,thalach,exang,oldpeak,slope,ca,thal)
-#     input = [age,sex,cp,trestbps,chol,fbs,restecg,thalach,exang,oldpeak,slope,ca,thal]
-#     # s = age + sex + cp + trestbps + chol + fbs + restecg + thalach + exang + oldpeak + slope + ca + thal
-#     # st.text(s)
-#     input = np.array(input)
-#     x = pred(input)
-#     st.text(x)
-#     # classify the target variable belong to which class
-
-
